da5fb1243ef2eb52128878f24a89ae3eb66a5809
Ivan Kniazkov <ivan@kniazkov.com>
2021-05-08 12:39:41 +0300

    Handling web event in Goat function

--- web_server.c
+++ web_server.c
@@ -25,29 +25,37 @@ with Goat standard library.  If not, see <http://www.gnu.org/licenses/>.
 #include "goat_data.h"
 #include "mongoose.h"
 #include <memory.h>
+#include <assert.h>
 
 static struct mg_serve_http_opts s_http_server_opts;
-
-static void event_handler(struct mg_connection *connection, int event, void *event_data, void *user_data)
-{
-    if (event == MG_EV_HTTP_REQUEST)
-	{
-        mg_serve_http(connection, (struct http_message *) event_data, s_http_server_opts);
-    }
-}
 
 typedef struct
 {
     struct mg_mgr manager;
     struct mg_connection *connection;
+    goat_function callback;
+    const goat_function_caller *function_caller;
+    const goat_allocator *allocator;
 } web_server_data;
+
+static void event_handler(struct mg_connection *connection, int event, void *event_data, void *user_data)
+{
+    web_server_data *me = (web_server_data*)user_data;
+    //assert(me != NULL);
+    if (event == MG_EV_HTTP_REQUEST)
+	{
+        if (me != NULL)
+            call_goat_function(me->function_caller, &me->callback, me->allocator, 0, NULL);
+        mg_serve_http(connection, (struct http_message *) event_data, s_http_server_opts);
+    }
+}
 
 goat_value * create_server(const goat_shell *shell, const goat_allocator *allocator, int arg_count, goat_value **arg_list)
 {
-    if (arg_count < 1)
+    if (arg_count < 2)
         return NULL;
 
-    if (!is_goat_number(arg_list[0]))
+    if (!is_goat_number(arg_list[0]) || arg_list[1]->type != goat_type_function)
         return NULL;
 
     int64_t port = goat_value_to_int64(arg_list[0]);
@@ -58,7 +66,8 @@ goat_value * create_server(const goat_shell *shell, const goat_allocator *alloca
     sprintf(port_str, "%ld", port);
 
     web_server_data *data = calloc(1, sizeof(web_server_data));
-    mg_mgr_init(&data->manager, NULL);
+    data->callback = *((goat_function*)arg_list[1]);
+    mg_mgr_init(&data->manager, data);
     printf("Starting web server on port %ld\n", port);
     data->connection = mg_bind(&data->manager, port_str, event_handler, data);
     if (data->connection == NULL)
@@ -79,38 +88,10 @@ goat_value * tick(const goat_shell *shell, const goat_allocator *allocator, int 
     if (arg_count > 0 && arg_list[0]->type == goat_type_raw_data)
     {
         goat_raw_data *arg = (goat_raw_data*)(arg_list[0]);
-        web_server_data *data = (web_server_data*)(arg->raw_data);
-        mg_mgr_poll(&data->manager, 1000);
+        web_server_data *me = (web_server_data*)(arg->raw_data);
+        me->function_caller = shell->function_caller;
+        me->allocator = allocator;
+        mg_mgr_poll(&me->manager, 1000);
     }
     return NULL;
 }
-
-/*
-int main(void)
-{
-    struct mg_mgr mgr;
-    struct mg_connection *nc;
-
-    mg_mgr_init(&mgr, NULL);
-    printf("Starting web server on port %s\n", s_http_port);
-    nc = mg_bind(&mgr, s_http_port, ev_handler);
-    if (nc == NULL)
-	{
-        printf("Failed to create listener\n");
-        return 1;
-    }
-
-    // Set up HTTP server parameters
-    mg_set_protocol_http_websocket(nc);
-    s_http_server_opts.document_root = ".";    // Serve current directory
-    s_http_server_opts.enable_directory_listing = "yes";
-
-    for (;;)
-	{
-        mg_mgr_poll(&mgr, 1000);
-    }
-    mg_mgr_free(&mgr);
-
-    return 0;
-}
-*/
